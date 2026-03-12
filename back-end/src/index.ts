5e03bc21dd7bdeb74cbaacef64c7a248e467e73a
kaungsan2001 <kaungsan2001.dev@gmail.com>
2026-03-12 11:36:14 +0630

    dotenv

--- back-end/src/index.ts
+++ back-end/src/index.ts
@@ -4,12 +4,14 @@ import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import routes from "./routes/index.js";
 import { errorHandler } from "./middlewares/errorMiddleware.js";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 8080;
 
 app.use(helmet());
-app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(cors({ origin: process.env.FRONTEND_URL, credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
