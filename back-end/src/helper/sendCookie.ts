42cebab2d1cdcd8e2e68b5fae1c9a15af99ff7f4
kaungsan2001 <kaungsan2001.dev@gmail.com>
2026-03-12 12:26:07 +0630

    cookie secure set to true

--- back-end/src/helper/sendCookie.ts
+++ back-end/src/helper/sendCookie.ts
@@ -3,8 +3,8 @@ import type { Response } from "express";
 export const sendCookie = (res: Response, authToken: string) => {
   return res.cookie("authToken", authToken, {
     httpOnly: true,
-    secure: false,
-    sameSite: "lax",
+    secure: true,
+    sameSite: "none",
     maxAge: 30 * 24 * 60 * 60 * 1000,
   });
 };
